fb83e1181e97dca321033bd3ab2d2bd4699890a2
Mansour Hamidi <mansourhamidi@hotmail.com>
2015-12-01 14:46:04 +0100

    heroku deployment

--- home/migrations/0001_initial.py
+++ home/migrations/0001_initial.py
@@ -13,11 +13,119 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='AnswerBase',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='AskBase',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('isAccepted', models.BooleanField()),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('category', models.CharField(max_length=255)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Question',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('question', models.CharField(max_length=255)),
+                ('question_type', models.CharField(default=b'radio', max_length=200, choices=[(b'radio', b'radio'), (b'select', b'select')])),
+                ('choices', models.TextField(help_text=b'if the question type is "radio," or "select,"', null=True, blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Response',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('updated', models.DateTimeField(auto_now=True)),
+                ('interview_uuid', models.CharField(max_length=36, verbose_name=b'Interview unique identifier')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Survey',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('description', models.TextField()),
+            ],
+        ),
+        migrations.CreateModel(
             name='UserProfile',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('picture', models.ImageField(upload_to=b'profile_images', blank=True)),
                 ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
+        ),
+        migrations.CreateModel(
+            name='AnswerRadio',
+            fields=[
+                ('answerbase_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='home.AnswerBase')),
+                ('body', models.TextField(null=True, blank=True)),
+            ],
+            bases=('home.answerbase',),
+        ),
+        migrations.CreateModel(
+            name='AnswerSelect',
+            fields=[
+                ('answerbase_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='home.AnswerBase')),
+                ('body', models.TextField(null=True, blank=True)),
+            ],
+            bases=('home.answerbase',),
+        ),
+        migrations.CreateModel(
+            name='CustomQuestion',
+            fields=[
+                ('question_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='home.Question')),
+                ('creator', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            bases=('home.question',),
+        ),
+        migrations.AddField(
+            model_name='response',
+            name='survey',
+            field=models.ForeignKey(to='home.Survey'),
+        ),
+        migrations.AddField(
+            model_name='response',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL, null=True),
+        ),
+        migrations.AddField(
+            model_name='question',
+            name='category_type',
+            field=models.ForeignKey(to='home.Category', null=True),
+        ),
+        migrations.AddField(
+            model_name='question',
+            name='survey',
+            field=models.ForeignKey(to='home.Survey', null=True),
+        ),
+        migrations.AddField(
+            model_name='answerbase',
+            name='question',
+            field=models.ForeignKey(to='home.Question'),
+        ),
+        migrations.AddField(
+            model_name='answerbase',
+            name='response',
+            field=models.ForeignKey(to='home.Response'),
+        ),
+        migrations.AddField(
+            model_name='askbase',
+            name='customQuestion',
+            field=models.ForeignKey(to='home.CustomQuestion', null=True),
         ),
     ]
